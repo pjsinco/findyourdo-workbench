f83a2e9bea44d0ac304d09875a5c398eb23c702a
pjsinco <psinco@gmail.com>
2016-01-11 11:03:33 -0600

    Form a search object from query string on results page

--- src/scripts/components/results-page.js
+++ src/scripts/components/results-page.js
@@ -3,10 +3,39 @@
 var React = require('react');
 
 
-
 var ResultsPage = React.createClass({
 
+  /**
+   * https://css-tricks.com/snippets/jquery/get-query-params-object/
+   * 
+   */
+  _getQueryParameters: function() {
+      var paramString = '?' + window.location.href.split('?')[1];
+  
+      return (paramString)
+          .replace(/(^\?)/,'')
+          .split("&")
+          .map(function(n) {
+              return n = n.split("="),this[n[0]] = decodeURIComponent(n[1]), this;
+          }.bind({}))[0];
+  },
+
+  propTypes: {
+    searchLocation: React.PropTypes.object,
+    onLocationChange: React.PropTypes.func,
+    onSubmit: React.PropTypes.func,
+  },
+
+  componentDidMount: function() {
+    var searchObject = this._getQueryParameters();
+
+    // TODO temp
+    console.dir(searchObject);
+    
+  },
+
   render: function() {
+
     return (
       <div />
     );
