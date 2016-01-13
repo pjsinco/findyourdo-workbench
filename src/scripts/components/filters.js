3e216bb081e09bb68b3b8c5dbfb9ce7c36070850
pjsinco <psinco@gmail.com>
2016-01-12 18:20:05 -0600

    Add a slider to filters

--- src/scripts/components/filters.js
+++ src/scripts/components/filters.js
@@ -1,12 +1,49 @@
 'use strict';
 
 var React = require('react');
+var ReactDOM = require('react-dom');
 
 var Filters = React.createClass({
+
+  _getMin: function() {
+    
+  },
+
+  _getMax: function() {
+    
+  },
+
+  _onMouseUp: function(evt) {
+console.log('onmouseup');
+console.log(evt.target.value);
+  },
+
+  _onChange: function(evt) {
+//console.log('slideronchange');    
+//console.log(evt.target.value);
+  },
+
+  _onInput: function(evt) {
+//console.log('slideroninput');    
+//console.log(evt.target.value);
+  },
 
   render: function() {
     return (
       <div className="filters">
+        <label htmlFor="radius">Distance</label>
+        <input onChange={this._onChange} 
+          onInput={this._onInput}
+          onMouseUp={this._onMouseUp}
+          type="range" 
+          name="radius" 
+          min={0} 
+          max={250} 
+          step={1} 
+          ref="radius"
+        />
+        <p>
+        </p>
       </div>
     );
   }
