376fb76711e4af3d55f0199d2c8f9613e23aaebd
pjsinco <psinco@gmail.com>
2016-01-13 10:55:56 -0600

    Make RadiusFilter component

--- src/scripts/components/filters.js
+++ src/scripts/components/filters.js
@@ -2,42 +2,12 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+var RadiusFilter = require('./filters/radius');
 var querystring = require('querystring');
 var url = require('url');
 
 var Filters = React.createClass({
 
-  _getMin: function() {
-    
-  },
-
-  _getMax: function() {
-    
-  },
-
-  _onMouseUp: function(evt) {
-    var radius = evt.target.value;
-console.log(radius);
-
-    var newUrl = {
-      protocol: window.location.protocol,
-      host: window.location.host,
-      pathname: '/find-your-do',
-      hash: 'search'
-    };
-
-    this.props.getDoctors(this.props.searchLocation, radius);
-
-
-//    if (searchLocation) {
-//      window.location = url.format(newUrl) + '?' + 
-//        querystring.stringify(searchLocation);
-//    }
-//    this.props.getDoctors(
-//
-//    );
-    
-  },
 
   _onChange: function(evt) {
 //console.log('slideronchange');    
@@ -52,19 +22,10 @@ console.log(radius);
   render: function() {
     return (
       <div className="filters">
-        <label htmlFor="radius">Distance</label>
-        <input onChange={this._onChange} 
-          onInput={this._onInput}
-          onMouseUp={this._onMouseUp}
-          type="range" 
-          name="radius" 
-          min={1} 
-          max={250} 
-          step={1} 
-          ref="radius"
+        <RadiusFilter 
+          searchLocation={this.props.searchLocation} 
+          getDoctors={this.props.getDoctors}
         />
-        <p>
-        </p>
       </div>
     );
   }
---
+++ src/scripts/components/filters/radius.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var React = require('react');
+
+var RadiusFilter = React.createClass({
+
+  propTypes: {
+    searchLocation: React.PropTypes.object,
+    getDoctors: React.PropTypes.func,
+  },
+
+  _getMin: function() {
+    
+  },
+
+  _getMax: function() {
+    
+  },
+
+  _onMouseUp: function(evt) {
+    var radius = evt.target.value;
+console.log(radius);
+
+    var newUrl = {
+      protocol: window.location.protocol,
+      host: window.location.host,
+      pathname: '/find-your-do',
+      hash: 'search'
+    };
+
+    this.props.getDoctors(this.props.searchLocation, radius);
+
+  },
+  render: function() {
+    return (
+      <div>
+        <label htmlFor="radius">Distance</label>
+        <input onChange={this._onChange} 
+          onInput={this._onInput}
+          onMouseUp={this._onMouseUp}
+          type="range" 
+          name="radius" 
+          min={1} 
+          max={250} 
+          step={1} 
+          ref="radius"
+        />
+        <p>
+        </p>
+      </div>
+    );
+  }
+
+});
+
+module.exports = RadiusFilter;
